a3db7a7741ee48c13387a322a4d49c960511ff32
mntmn <lukas@mnt.mn>
2015-09-04 14:27:43 +0200

    fix cache option

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use("/uploads", express.static(__dirname + '/uploads'));
 app.use("/static", express.static(__dirname + '/static'));
 
 var swig  = require('swig');
-swig.setDefaults({ cache: true });
+swig.setDefaults({ cache: "memory" });
 
 app.engine('html', swig.renderFile);
 
